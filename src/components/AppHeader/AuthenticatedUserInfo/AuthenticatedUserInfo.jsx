38ddc696f80eb26d28974773acce6fde9c3f6a8f
kumaradji <kumaradji@gmail.com>
2024-01-21 16:03:44 +0300

    хедер авторизованный пользователь

--- src/components/AppHeader/AuthenticatedUserInfo/AuthenticatedUserInfo.jsx
+++ src/components/AppHeader/AuthenticatedUserInfo/AuthenticatedUserInfo.jsx
@@ -1,18 +1,62 @@
-// AuthenticatedUserInfo.jsx
 import React from 'react';
 import styles from './AuthenticatedUserInfo.module.scss';
 import AvatarImg from '../../../assets/images/Avatar.svg';
+import userPanelRect from '../../../assets/images/userPanelRect.svg';
+import Loader from "../../Loader/Loader";
 
-const AuthenticatedUserInfo = ({ userInfo, handleLogout }) => (
-  <div className={styles.userInfo}>
+const AuthenticatedUserInfo = ({ userInfo, handleLogout }) => {
+  const isLoggedIn = !!userInfo;
+  const loading = false;
+  const usedCompanies = 34;
+  const companyLimit = 100;
 
-    {/* Вывод данных о пользователе */}
+  return (
+    <div className={styles.userInfo}>
+      <div className={styles.userInfo__userPanel}>
+        <img
+          className={styles.userInfo__userPanelRect}
+          src={userPanelRect}
+          alt="userPanelRect"
+        />
 
-    <div onClick={handleLogout} className={styles.logoutText}>
-      Выйти
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            {isLoggedIn && (
+              <div className={styles.userInfo__companyInfo}>
+                <div className={styles.userInfo__companyInfoUsed}>
+                  {usedCompanies}
+                </div>
+                <div className={styles.userInfo__companyInfoLimit}>
+                  {companyLimit}
+                </div>
+                <div className={styles.userInfo__companyInfoTextUsed}>
+                  Использовано компаний
+                </div>
+                <div className={styles.userInfo__companyInfoTextLimit}>
+                  Лимит по компаниям
+                </div>
+              </div>
+            )}
+          </>
+        )}
+      </div>
+
+      <div
+        onClick={handleLogout}
+        className={styles.userInfo__logoutText}
+      >
+        Выйти
+      </div>
+
+      <img
+        src={AvatarImg}
+        alt="Аватар"
+        className={styles.userInfo__avatar}
+      />
     </div>
-    <img src={AvatarImg} alt="Аватар" className={styles.avatar}/>
-  </div>
-);
+  );
+};
 
 export default AuthenticatedUserInfo;
