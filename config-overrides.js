d2cec4586121c89b1f13a3c2b5113b0b0cbfefbc
kumaradji <kumaradji@gmail.com>
2024-01-28 16:04:19 +0300

    Отключил webpack, применил обратно config-overrides.js разбираюсь с футором. Стили для Heder и footer нужно пересмотреть.

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+// config-overrides.js
+
+const {override, addWebpackModuleRule} = require("customize-cra");
+
+module.exports = override(
+  addWebpackModuleRule({
+    test: /\.svg$/,
+    use: ["@svgr/webpack", "url-loader"]
+  })
+);
