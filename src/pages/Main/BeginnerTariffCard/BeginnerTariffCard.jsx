70613695d79a03e2c3798ec7f2419b086dee9bcf
kumaradji <kumaradji@gmail.com>
2024-02-06 16:56:55 +0300

    ++ БЭМ

--- src/pages/Main/BeginnerTariffCard/BeginnerTariffCard.jsx
+++ src/pages/Main/BeginnerTariffCard/BeginnerTariffCard.jsx
@@ -1,58 +1,73 @@
 // BeginnerTariffCard/BeginnerTariffCard.jsx
 
-import React from 'react';
-import styles from './BeginnerTariffCard.module.scss';
-import LightBulbIcon from '../../../assets/icons/LightBulbTariff.svg';
-import ItemsList from '../../../assets/images/BigginerTariffList.jpeg';
+import React, {useState} from "react";
+import styles from "./BeginnerTariffCard.module.scss";
+import LightBulbIcon from "../../../assets/icons/LightBulbTariff.svg";
+import ItemsList from "../../../assets/images/BigginerTariffList.jpeg";
 
 function BeginnerTariffCard() {
-  const handleMoreButtonClick = () => {
-    // Пустая функция-заглушка для события клика кнопки
+
+  const [isActive, setIsActive] = useState(false);
+  const onMouseDown = () => {
+    setIsActive(true);
+  };
+  const onMouseUp = () => {
+    setIsActive(false);
   };
 
   return (
-    <section className={styles.container}>
-      <div className={styles.container__orangeTop}/>
-      <img className={styles.container__orangeTop_iconLightBulb} src={LightBulbIcon} alt="Lightbulb-Icon"/>
+    <section className={styles.beginnerTariffCard}>
+      <div className={styles.beginnerTariffCard__orangeTop}/>
+      <img
+        className={styles.beginnerTariffCard__orangeTop_icon}
+        src={LightBulbIcon}
+        alt="Lightbulb-Icon"
+      />
 
-      <div className={styles.container__title}>
-        Beginner
-      </div>
+      <div className={styles.beginnerTariffCard__title}>Beginner</div>
 
-      <div className={styles.container__blueStripe}/>
-      <div className={styles.container__currentTariffLabel}>
-        Текущий тариф
-      </div>
+      <div className={styles.beginnerTariffCard__blueStripe}/>
 
-      <div className={styles.container__personalCabinetButton} onClick={handleMoreButtonClick}>
-        <div className={styles.container__personalCabinetButton_personalCabinetText}>
+      <div
+        className={`
+          ${styles.beginnerTariffCard__personalCabinetButton} 
+          ${isActive && styles.beginnerTariffCard__personalCabinetButton_active}
+        `}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+      >
+        <div className={styles.beginnerTariffCard__personalCabinetButton_text}>
           Перейти в личный кабинет
         </div>
       </div>
 
-      <div className={styles.container__infoTextTitle}>
+      <div className={styles.beginnerTariffCard__infoTitle}>
         Для небольшого исследования
       </div>
 
-      <div className={styles.container__currentTariffLabel}>
+      <div className={styles.beginnerTariffCard__currentTariffLabel}>
         Текущий тариф
       </div>
 
-      <div className={styles.container__priceTitle}>
-        <div className={styles.container__priceTitle_infoText}>
-          <span className={styles.container__priceTitle_price}>
+      <div className={styles.beginnerTariffCard__priceTitle}>
+        <div className={styles.beginnerTariffCard__priceTitle_infoText}>
+          <span className={styles.beginnerTariffCard__priceTitle_price}>
             799 ₽
           </span>
-          <span className={styles.container__priceTitle_priceHigh}>
+          <span className={styles.beginnerTariffCard__priceTitle_priceHigh}>
             1 200 ₽
           </span>
-          <p className={styles.container__priceTitle_currency}>
+          <p className={styles.beginnerTariffCard__priceTitle_currency}>
             или 150 ₽/мес. при рассрочке на 24 мес.
           </p>
         </div>
       </div>
 
-      <img className={styles.container__tariffItemsList} src={ItemsList} alt="ItenmsList"/>
+      <img
+        className={styles.beginnerTariffCard__tariffItemsList}
+        src={ItemsList}
+        alt="ItenmsList"
+      />
     </section>
   );
 }
